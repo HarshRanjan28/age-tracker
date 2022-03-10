68b84a5a192717010b00de1c6c62eb852888f7f5
HarshRanjan28 <harshranjan2897@gmail.com>
2022-03-10 11:34:42 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [age, setAge] = useState(0);
+
+  const getAge = () => {
+    const today = new Date();
+    const birthday = new Date(document.getElementById("date-input").value);
+
+    const birthDate = birthday.getDate();
+    const birthMonth = birthday.getMonth();
+    const birthYear = birthday.getFullYear();
+
+    const currentDay = today.getDate();
+    const currentMonth = today.getMonth();
+    const currentYear = today.getFullYear();
+
+    const yourAge = currentYear - birthYear;
+
+    if (
+      birthYear > currentYear ||
+      (birthYear === currentYear && birthMonth > currentMonth) ||
+      (birthDate > currentDay &&
+        currentMonth === birthMonth &&
+        currentYear === birthYear)
+    ) {
+      alert("Your are not born yet");
+    } else if (birthMonth > currentMonth || birthDate > currentDay) {
+      setAge(yourAge - 1);
+    } else {
+      setAge(yourAge);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="age-tracker">
+        <h1>Age-Tracker</h1>
+        <div className="age-details">
+          <label htmlFor="name">Enter Your Birth Date</label>
+          <input type="date" id="date-input" />
+
+          <button className="btn" onClick={getAge}>
+            Submit
+          </button>
+          <h2>Your Age is {age} Years</h2>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
